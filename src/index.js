890c368b171145f2497a648bc54d7b82c425bd70
tinsaye-simeneh <tinsayesimeneh608@gmail.com>
2022-07-01 09:39:25 +0300

    minor bug fix

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React, {useState} from 'react';
 import ReactDOM from 'react-dom/client';
 import{BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Footer from './components/Footer';
-import Header from './components/header';
+import Header from './components/Header';
 import About from './components/pages/About'
 import Contact from './components/pages/Contact'
 import Home from './components/pages/Home'
